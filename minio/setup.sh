3ad3b6dafb736520c6a6951c0ff68bc33f111f05
Richard Pablo <richard.pablo@synchronoss.com>
2017-02-12 18:43:21 +0000

    minio: Moving inline script into its own setup file, reducing VM specs, adding minio docker command

---
+++ minio/setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "INFO: Installing Docker..."
+sudo apt-get update
+sudo apt -y install docker.io
+
+echo "INFO: Adding current user to docker..."
+sudo usermod -aG docker ubuntu
+
+echo "INFO: Pulling minio..."
+docker pull minio/minio
+
+echo "INFO: Running minio server..."
+docker run -d --name minio \
+--net=host \
+-e "MINIO_ACCESS_KEY=O9RGDJAYKPPIRUS7F6K2" \
+-e "MINIO_SECRET_KEY=AUagMf7zIuzTd9ZAeXmS658VY8R4DKdVaKnt9w3j" \
+-v /mnt/export/minio:/export \
+-v /mnt/config/minio:/root/.minio \
+minio/minio server http://192.167.201.101/export http://192.167.201.102/export http://192.167.201.103/export http://192.167.201.104/export
+
+date > /etc/vagrant_provisioned_at
