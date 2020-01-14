543f10892e4e5f609ebb1567c24eee35d366e7cd
Richard Pablo <richardpablop@gmail.com>
2020-01-14 18:57:07 +0000

    Updating Devbox

--- devbox/setup.sh
+++ devbox/setup.sh
@@ -4,9 +4,9 @@ echo "Installing Docker..."
 sudo apt -y install docker.io
 
 echo "Adding current user to docker..."
-sudo usermod -aG docker ubuntu
+sudo usermod -aG docker vagrant
 
 echo "Copying ssh keys into .ssh directory..."
-su ubuntu -c "cp -rn /host-ssh/* /home/ubuntu/.ssh/"
+su ubuntu -c "cp -rn /host-ssh/* /home/vagrant/.ssh/"
 
 date > /etc/vagrant_provisioned_at
